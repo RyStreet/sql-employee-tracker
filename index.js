f90ade98289e563b97f7aa3374273dcd2dffdfa9
Ryan Street <ryanstreet122@gmail.com>
2022-12-07 17:19:52 -0500

    view/add for depts, roles and employees functional, working on update employee function

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ const addDepartment = () => {
   .then(answers =>{
     const newDeptName = answers.deptName;
     const newDeptId = answers.deptId;
-    db.query(`INSERT into departments (id, department_name) VALUES (?, ?)`, newDeptId, newDeptName, (err, results) => {
+    db.query(`INSERT into departments (id, department_name) VALUES (?, ?)`, [newDeptId, newDeptName], (err, results) => {
       if (err){
         throw err
       }
@@ -103,7 +103,13 @@ const addDepartment = () => {
 };
 
 viewAllRoles = () =>{
-  db.query("SELECT * FROM roles", function (err, results){
+  db.query(`select roles.id, 
+  roles.title, 
+  roles.salary, 
+  departments.department_name as department 
+  from roles 
+  left join departments on roles.department_id = departments.id`
+  , function (err, results){
     if (err){
       throw err
     }
@@ -158,7 +164,7 @@ message: "Enter Salary",
   const id = answers.roleId;
   const salary = answers.salary;
   const deptId = answers.deptId;
-  db.query(`INSERT into roles (id, title, salary, department_id) VALUES (${id}, "${name}", ${salary}, ${deptId} )`, function (err, results){
+  db.query(`INSERT into roles (id, title, salary, department_id) VALUES (?,?,?,? )`, [id, name, salary, deptId], (err, results) => {
     if (err){
       throw err
     }
@@ -166,3 +172,84 @@ message: "Enter Salary",
     return viewAllRoles();
 })
 })};
+
+viewAllEmployees = () =>{
+  db.query(`SELECT employees.id, 
+    employees.first_name, 
+    employees.last_name,
+    roles.title AS title,
+    roles.salary AS salary,
+    departments.department_name AS department,
+    CONCAT (manager.first_name, " ", manager.last_name) AS manager 
+FROM employees
+LEFT JOIN roles ON employees.role_id = roles.id
+LEFT JOIN departments ON roles.department_id = departments.id
+LEFT JOIN employees manager ON employees.manager_id = manager.id`
+, function (err, results){
+  if (err){
+    throw err
+  }
+  console.table(results)
+  return inquirer
+  .prompt([{
+    type: "list",
+    name: 'employeeList',
+    choices: ["Back", "Add Employee"]
+  }])
+  .then(answers =>{ 
+    const roleChoice = answers.employeeList;
+    if(roleChoice === "Back"){
+      displayTracker();
+    }
+    if(roleChoice === "Add Employee"){
+      addEmployee();
+    }
+})})};
+
+addEmployee = () => {
+  return inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'firstName',
+      message: 'Enter Employee First Name',
+    },
+    {
+    type: 'input',
+    name: 'lastName',
+    message: "Enter Employee Last Name",
+    },
+    
+    {
+    type: 'input',
+    name: 'id',
+    message: "Enter Employee ID",
+    },
+
+    {
+      type: 'input',
+      name: 'role_id',
+      message: "Enter Employee Role ID",
+      },
+    
+    {
+      type: 'input',
+      name: 'manager_id',
+      message: "Enter Manager ID (If Manager enter 0)",  
+    },
+])
+.then(answers => {
+const first_name = answers.firstName;
+const last_name = answers.lastName;
+const id = answers.id;
+const role_id = answers.role_id;
+const manager_id = answers.manager_id;
+db.query(`INSERT into employees (id, first_name, last_name, role_id, manager_id) VALUES (?,?,?,?,?)`, [id, first_name, last_name, role_id, manager_id], (err, results) => {
+if(err){
+  throw err
+}
+console.table(results)
+return viewAllEmployees();
+})
+})
+};
